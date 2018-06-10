5e7459941e562223cd6cdda882b4500e9103fd87
Hugo Gilbert <gilbert.hugo.78@gmail.com>
2018-06-10 17:32:31 +0200

    improve player

--- BrunchHell/Assets/Script/PlayerBehaviourScript.cs
+++ BrunchHell/Assets/Script/PlayerBehaviourScript.cs
@@ -7,14 +7,30 @@ public class PlayerBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        //float width = GetComponent<SpriteRenderer>.bounds.size.x;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //float radius = this.gameObject.GetComponent<CircleCollider2D>().radius;
+        //float sx = transform.localScale.x;
+
         transform.localPosition = CameraScript.GetCamera().ScreenToWorldPoint(new Vector3(
             Math.Max(Math.Min(Input.mousePosition.x, CameraScript.GetCamera().pixelWidth),0), 
             Math.Max(Math.Min(Input.mousePosition.y, CameraScript.GetCamera().pixelHeight),0), 
             10));
+
+        //Destroy(this.gameObject);
     }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.CompareTag("enemyShot") || other.CompareTag("enemy") || true){
+            Destroy(this.gameObject);   
+            Destroy(this);   
+        }
+    }
+
+
 }
