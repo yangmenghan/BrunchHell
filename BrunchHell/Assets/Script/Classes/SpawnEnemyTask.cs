d06ea37387236f8f1ffd953741e4d3bf4c9ed4b6
codefrommars <codefrommars@gmail.com>
2018-06-10 17:24:31 +0200

    Merged conflicts

---
+++ BrunchHell/Assets/Script/Classes/SpawnEnemyTask.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemyTask : GameManagerTask{
+
+    private GameObject prefab;
+    private Vector2 pos;
+
+    public SpawnEnemyTask(GameObject manager,  GameObject prefab, Vector2 pos) : base(manager)
+    {
+        this.prefab = prefab;
+        this.pos = pos;
+    }
+
+	override public bool Update () {
+        GameObject.Instantiate(prefab, new Vector3(pos.x, pos.y, 0), Quaternion.identity);
+        return true;
+    }
+}
---
+++ BrunchHell/Assets/Script/Classes/WaitTask.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitTask : GameManagerTask{
+
+  
+    private float time;
+    private float total;
+
+    public WaitTask(GameObject manager, float time) : base(manager)
+    {
+        this.time = time;
+    }
+
+    override public bool Update()
+    {
+        total += Time.deltaTime;
+        if( total > time )
+            return true;
+
+        return false;
+    }
+}
